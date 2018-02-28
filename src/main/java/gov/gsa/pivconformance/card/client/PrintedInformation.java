70bf4deeca6745e0730778ed02787ff60e97cca6
Armen Galustyan <armen@redhoundsoftware.com>
2018-02-28 12:48:03 -0500

    Adding skeleton classed for each Data Element.  Updated PIVDataObjectFactory to return appropriate class based on the oid.

---
+++ src/main/java/gov/gsa/pivconformance/card/client/PrintedInformation.java
@@ -0,0 +1,11 @@
+package gov.gsa.pivconformance.card.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrintedInformation extends PIVDataObject {
+    // slf4j will thunk this through to an appropriately configured logging library
+    private static final Logger s_logger = LoggerFactory.getLogger(PrintedInformation.class);
+
+
+}
