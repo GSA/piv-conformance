e7f08551984304dda752b24b8278bd614e8ca802
Geoff Beier <geoff@redhoundsoftware.com>
2018-02-28 09:20:23 -0500

    beginnings of a set of constants useful with the BerTlv classes for processing tags.

---
+++ src/main/java/gov/gsa/pivconformance/tlv/TagConstants.java
@@ -0,0 +1,12 @@
+package gov.gsa.pivconformance.tlv;
+
+public class TagConstants {
+    public static final byte[] AID_TAG = { 0x4F };
+    public static final byte[] APPLICATION_LABEL = { 0x50 };
+    public static final byte[] UNIFORM_RESOURCE_LOCATOR = { 0x50 };
+    public static final byte[] CRYPTOGRAPHIC_ALGORITHMS = { (byte) 0xAC };
+    public static final byte[] CRYPTOGRAPHIC_ALGORITHM_IDENTIFIER = { (byte) 0x80 };
+    public static final byte[] OBJECT_IDENTIFIER = { (byte) 0x06 };
+    public static final byte[] TAG_LIST = { 0x5c };
+
+}
