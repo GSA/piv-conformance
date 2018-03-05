768204e1de59be74d153d07ad21279d89634b132
Geoff Beier <geoff@redhoundsoftware.com>
2018-03-05 12:55:27 -0500

    sample of parsing (with no real error checking or anything yet)

--- src/main/java/gov/gsa/pivconformance/card/client/X509CertificateForPIVAuthentication.java
+++ src/main/java/gov/gsa/pivconformance/card/client/X509CertificateForPIVAuthentication.java
@@ -1,18 +1,17 @@
 package gov.gsa.pivconformance.card.client;
 
-import gov.gsa.pivconformance.tlv.BerTlv;
-import gov.gsa.pivconformance.tlv.BerTlvs;
-import gov.gsa.pivconformance.tlv.BerTlvParser;
-import gov.gsa.pivconformance.tlv.CCTTlvLogger;
+import gov.gsa.pivconformance.tlv.*;
 import gov.gsa.pivconformance.tools.PIVRunner;
 import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.security.cert.X509Certificate;
 import java.security.cert.CertificateFactory;
 import java.util.List;
 import java.io.ByteArrayInputStream;
+import java.util.zip.GZIPInputStream;
 
 public class X509CertificateForPIVAuthentication extends PIVDataObject {
     // slf4j will thunk this through to an appropriately configured logging library
@@ -42,16 +41,36 @@ public class X509CertificateForPIVAuthentication extends PIVDataObject {
                         s_logger.info("Tag {}: {}", Hex.encodeHexString(tlv.getTag().bytes), Hex.encodeHexString(tlv.getBytesValue()));
 
                         //BerTlvs values3 = tp.parse(tlv.getBytesValue());
-                        BerTlv outer2 = tp.parseConstructed(tlv.getBytesValue());
-                        List<BerTlv> values2 = outer2.getValues();
-                        for(BerTlv tlv2 : values) {
-                            if(tlv.isPrimitive()) {
-                                s_logger.info("Tag {}: {}", Hex.encodeHexString(tlv.getTag().bytes), Hex.encodeHexString(tlv.getBytesValue()));
+                        BerTlvs outer2 = tp.parse(tlv.getBytesValue());
+                        List<BerTlv> values2 = outer2.getList();
+                        byte[] rawCertBuf = null;
+                        byte[] certInfoBuf = null;
+                        for(BerTlv tlv2 : values2) {
+                            if(tlv2.isPrimitive()) {
+                                s_logger.info("Tag {}: {}", Hex.encodeHexString(tlv2.getTag().bytes), Hex.encodeHexString(tlv2.getBytesValue()));
+                            } else {
+                                if(tlv2.getTag().bytes[0] == 0x70) {
+                                    if (tlv2.hasRawValue()) {
+                                        rawCertBuf = tlv2.getBytesValue();
+                                        s_logger.info("Tag {}: {}", Hex.encodeHexString(tlv2.getTag().bytes), Hex.encodeHexString(rawCertBuf));
+                                    }
+                                }
+                                if(tlv2.getTag().bytes[0] == 0x71) {
+                                    certInfoBuf = tlv2.getBytesValue();
+                                    s_logger.info("Got cert info buffer: {}", Hex.encodeHexString(certInfoBuf));
+                                }
                             }
                         }
 
+                        InputStream certIS = null;
+                        if(certInfoBuf != null && certInfoBuf[0] == 0x01) {
+                            certIS = new GZIPInputStream(new ByteArrayInputStream(rawCertBuf));
+                        } else {
+                            certIS = new ByteArrayInputStream(rawCertBuf);
+                        }
                         CertificateFactory cf = CertificateFactory.getInstance("X509");
-                        X509Certificate m_pivAuthCert = (X509Certificate)cf.generateCertificate(new ByteArrayInputStream(tlv.getBytesValue()));
+                        X509Certificate m_pivAuthCert = (X509Certificate)cf.generateCertificate(certIS);
+                        s_logger.info(m_pivAuthCert.getSubjectDN().toString());
                     } else {
                         s_logger.info("Object: {}", Hex.encodeHexString(tlv.getTag().bytes));
                     }
