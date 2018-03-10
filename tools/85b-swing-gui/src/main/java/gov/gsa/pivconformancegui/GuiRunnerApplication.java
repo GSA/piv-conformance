dc9aaf7e217a19261da655758c345284ce8536d8
Geoff Beier <geoff@redhoundsoftware.com>
2018-03-09 21:15:10 -0500

    roll back tree and move to new package names for gui bits

---
+++ tools/85b-swing-gui/src/main/java/gov/gsa/pivconformancegui/GuiRunnerApplication.java
@@ -0,0 +1,86 @@
+package gov.gsa.pivconformancegui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
+public class GuiRunnerApplication {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiRunnerApplication window = new GuiRunnerApplication();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiRunnerApplication() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnFile.add(mntmOpen);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		JMenuItem mntmCut = new JMenuItem("Cut");
+		mnEdit.add(mntmCut);
+		
+		JMenuItem mntmCopy = new JMenuItem("Copy");
+		mnEdit.add(mntmCopy);
+		
+		JMenuItem mntmPaste = new JMenuItem("Paste");
+		mnEdit.add(mntmPaste);
+		
+		JMenu mnView = new JMenu("View");
+		menuBar.add(mnView);
+		
+		JRadioButtonMenuItem rdbtnmntmSimplified = new JRadioButtonMenuItem("Simplified");
+		mnView.add(rdbtnmntmSimplified);
+		
+		JRadioButtonMenuItem rdbtnmntmAdvanced = new JRadioButtonMenuItem("Advanced");
+		mnView.add(rdbtnmntmAdvanced);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAboutPivCard = new JMenuItem("About PIV Card Conformance Tool");
+		mnHelp.add(mntmAboutPivCard);
+	}
+
+}
