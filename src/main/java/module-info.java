846b9ce303b5f24a2d76aac1f815aa53c719498a
Armen Galustyan <armen@redhoundsoftware.com>
2018-03-15 10:21:55 -0400

    Geoff figured out what needed to go into module-info.java to make IDE happy with bouncycastle  modules.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,7 @@ open module gov.gsa.pivconformance {
     requires org.apache.commons.codec;
     requires commons.cli;
     requires jmrtd;
+    requires bcpkix.jdk15on;
+    requires bcprov.jdk15on;
+
 }
