a21a71a0ed949a4031dc8b6847a20c3e8c0185da
Geoff Beier <geoff@redhoundsoftware.com>
2018-04-06 06:12:24 -0400

    Add utility functions for conformance harness

---
+++ src/main/java/gov/gsa/pivconformance/tools/ConformanceTestRunner.java
@@ -0,0 +1,4 @@
+package gov.gsa.pivconformance.tools;
+
+public class ConformanceTestRunner {
+}
--- src/main/java/gov/gsa/pivconformance/utils/PCSCUtils.java
+++ src/main/java/gov/gsa/pivconformance/utils/PCSCUtils.java
@@ -3,9 +3,14 @@ package gov.gsa.pivconformance.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.TerminalFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class PCSCUtils {
@@ -35,5 +40,29 @@ public class PCSCUtils {
             s_logger.error("Unable to read " + fileName.getAbsolutePath(), e);
             return;
         }
+    }
+    public static List<String> GetConnectedReaders() {
+        ArrayList<String> readerList = new ArrayList<>();
+        TerminalFactory tf = TerminalFactory.getDefault();
+        List<CardTerminal> terminals = null;
+        try {
+            s_logger.debug("About to list connected readers");
+            terminals = tf.terminals().list();
+            s_logger.debug("Done listing connected readers");
+        } catch (CardException e) {
+            s_logger.error("Failed to list card terminals", e);
+            return readerList;
+        }
+        if(terminals.size() == 0) {
+            s_logger.debug("No readers were connected.");
+            return readerList;
+        }
+        int terminalCount = 0;
+        for(CardTerminal t : terminals) {
+            terminalCount++;
+            readerList.add(t.getName());
+        }
+        s_logger.debug("Found {} readers.", terminalCount);
+        return readerList;
     }
 }
