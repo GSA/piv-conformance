c9a46e326cbf46ba089e636da6e0321f2c406052
Geoff Beier <geoff@redhoundsoftware.com>
2019-03-14 16:06:00 -0400

    Adding missing schema to resources tree, pulling db functionality that was duplicated (with a different error each place) into a separate class

---
+++ conformancelib/src/main/java/gov/gsa/conformancelib/configuration/ConformanceTestDatabase.java
@@ -0,0 +1,57 @@
+package gov.gsa.conformancelib.configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConformanceTestDatabase {
+	private static final Logger s_logger = LoggerFactory.getLogger(ConformanceTestDatabase.class);
+	public ConformanceTestDatabase(Connection conn) {
+		setConnconnection(conn);
+	}
+	
+	public Connection getConnection() {
+		return m_conn;
+	}
+
+	public void setConnconnection(Connection conn) {
+		m_conn = conn;
+	}
+	
+	public void populateDefault() throws ConfigurationException {
+		if(m_conn == null) {
+			s_logger.error("Attempting to populate conformance test database without a JDBC connection");
+			throw new ConfigurationException("ConformanceTestDatabase cannot be populated without a JDBC connection.");
+		}
+		InputStream sqlIS = this.getClass().getResourceAsStream("/conformance-schema.sql");
+		Reader r = new InputStreamReader(sqlIS);
+		if(sqlIS == null) {
+			s_logger.error("Unable to load conformance-schema.sql from classpath.");
+			throw new ConfigurationException("ConformanceTestDatabase cannot load sql file from classpath.");
+		}
+		ScriptRunner sr = new ScriptRunner(m_conn);
+		sr.setAutoCommit(true);
+		sr.setStopOnError(true);
+		try {
+			sr.runScript(r);
+		} catch(Exception e) {
+			s_logger.error("Unable to populate database with defaults", e);
+			throw new ConfigurationException("Failed to execute SQL script", e);
+		}
+		try {
+			sqlIS.close();
+		} catch (IOException e) {
+			s_logger.error("Caught IOException while closing sql file", e);
+			throw new ConfigurationException("Failed to close sql file", e);
+		}
+	}
+
+	private Connection m_conn;
+}
---
+++ conformancelib/src/main/resources/conformance-schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS "TestSteps" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`Description`	TEXT,
+        `Class`         TEXT,
+        `Method`        TEXT,
+	`NumParameters`	INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS "TestsToSteps" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`TestStepId`	INTEGER,
+	`TestId`	INTEGER,
+        `Status`        INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS "TestStepParameters" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`TestStepId`	INTEGER,
+	`TestId`	INTEGER,
+        `Value`         TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "TestCases" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`TestCaseIdentifier`	TEXT,
+        `TestCaseDescription`   TEXT,
+        `Status`        INTEGER,
+        `ExpectedStatus` INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS "TestGroups" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+        `GroupDescription`   TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "GroupsToTestCases" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`TestGroupId`	INTEGER,
+	`TestCaseId`	INTEGER
+);
+
+-- this isn't really going to be used by the look of things. probably should
+-- be eliminated
+CREATE TABLE IF NOT EXISTS "SystemSettings" (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`ReaderName`	TEXT,
+	`ApplicationPIN`	TEXT,
+	`OutputDirectory`	TEXT,
+	`SettingsGroup`	TEXT,
+	`GPMasterKey`	TEXT
+);
+
