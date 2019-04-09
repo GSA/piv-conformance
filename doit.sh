0cfadb3b9c498f52f1da82d139c0029965ff8ec5
Bob Fontana <bob.fontana@certipath.com>
2019-04-09 10:27:06 -0400

    Number of Eclipse files plus another doit.sh

---
+++ doit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+pushd cardlib >/dev/null 2>&1
+	./gradlew clean
+	./gradlew eclipse
+	./gradlew install
+popd >/dev/null 2>&1
+
+pushd conformancelib >/dev/null 2>&1
+	./gradlew clean
+	./gradlew eclipse
+	./gradlew shadowJar
+popd >/dev/null 2>&1
+
+pushd tools/85b-swing-gui 2>&1
+	./gradlew clean
+	./gradlew eclipse
+	./gradlew shadowJar
+	./ensuredeps.sh
+popd >/dev/null 2>&1
