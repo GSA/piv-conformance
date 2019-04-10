679381868253074a070f8272a39a53a53b1fe40b
Geoff Beier <geoff@redhoundsoftware.com>
2019-04-10 12:43:22 -0400

    improve logging for test results

--- tools/85b-swing-gui/src/main/java/gov/gsa/pivconformancegui/GuiTestListener.java
+++ tools/85b-swing-gui/src/main/java/gov/gsa/pivconformancegui/GuiTestListener.java
@@ -1,7 +1,11 @@
 package gov.gsa.pivconformancegui;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
 
+import javax.net.ssl.SSLEngineResult.Status;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
@@ -20,9 +24,14 @@ public class GuiTestListener implements TestExecutionListener {
 	private static final Logger s_logger = LoggerFactory.getLogger(GuiTestListener.class);
 	private static final Logger s_testProgressLogger = LoggerFactory.getLogger("gov.gsa.pivconformance.testProgress");
 	private static final Logger s_testResultLogger = LoggerFactory.getLogger("gov.gsa.pivconformance.testResults");
+	private static final Logger s_AtomResultLogger = LoggerFactory.getLogger("gov.gsa.pivconformance.testAtomResults");
 	
 	private String m_testCaseIdentifier;
 	private JProgressBar m_progressBar;
+	
+	Map<TestIdentifier, TestExecutionResult> m_testStepResults;
+	boolean m_AtomFailed;
+	boolean m_AtomAborted;
 
 	@Override
 	public void testPlanExecutionStarted(TestPlan testPlan) {
@@ -41,6 +50,7 @@ public class GuiTestListener implements TestExecutionListener {
 	public void testPlanExecutionFinished(TestPlan testPlan) {
 		TestExecutionListener.super.testPlanExecutionFinished(testPlan);
 		s_testProgressLogger.info("Test plan finished for conformance test {}", m_testCaseIdentifier);
+		s_testResultLogger.info("{},{}", m_testCaseIdentifier, (m_AtomAborted||m_AtomFailed) ? "Fail" : "Pass"); 
 		try {
 			SwingUtilities.invokeAndWait(() -> {
 				m_progressBar.setString(m_testCaseIdentifier + " Finished.");
@@ -55,6 +65,7 @@ public class GuiTestListener implements TestExecutionListener {
 	public void executionStarted(TestIdentifier testIdentifier) {
 		TestExecutionListener.super.executionStarted(testIdentifier);
 		String displayName = testIdentifier.getDisplayName();
+		//if(!testIdentifier.isTest()) return;
 		if(displayName != "JUnit Jupiter") {
 			s_testProgressLogger.info("Started {}:{}", m_testCaseIdentifier, displayName);
 			try {
@@ -72,6 +83,21 @@ public class GuiTestListener implements TestExecutionListener {
 		TestExecutionListener.super.executionFinished(testIdentifier, testExecutionResult);
 		
 		String displayName = testIdentifier.getDisplayName();
+		//if(!testIdentifier.isTest()) return;
+		m_testStepResults.put(testIdentifier, testExecutionResult);
+		if(testExecutionResult.getStatus() == TestExecutionResult.Status.FAILED) {
+			m_AtomFailed = true;
+			s_testProgressLogger.error("Test atom {}:{} failed", m_testCaseIdentifier, displayName);
+		}
+		if(testExecutionResult.getStatus() == TestExecutionResult.Status.ABORTED) {
+			m_AtomAborted = true;
+			s_testProgressLogger.error("Test atom {}:{} aborted", m_testCaseIdentifier, displayName);
+		}
+		if(testExecutionResult.getStatus() == TestExecutionResult.Status.SUCCESSFUL) {
+			s_AtomResultLogger.info("{}:{},pass",m_testCaseIdentifier, displayName);
+		}
+		Optional<Throwable> exception = testExecutionResult.getThrowable();
+		if(exception.isPresent()) s_logger.error("Caused by:", exception.get());
 		if(displayName != "JUnit Jupiter") {
 			s_testProgressLogger.info("Finished {}:{}", m_testCaseIdentifier, displayName);
 		}
@@ -84,6 +110,7 @@ public class GuiTestListener implements TestExecutionListener {
 	}
 
 	public GuiTestListener() {
+		m_testStepResults = new LinkedHashMap<TestIdentifier, TestExecutionResult>();
 	}
 
 	public String getTestCaseIdentifier() {
