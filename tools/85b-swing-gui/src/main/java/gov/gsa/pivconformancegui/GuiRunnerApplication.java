524dae25e4cd48801a4a50b4d600757c40b91b29
Geoff Beier <geoff@redhoundsoftware.com>
2019-04-29 09:38:53 -0400

    missed set properties

--- tools/85b-swing-gui/src/main/java/gov/gsa/pivconformancegui/GuiRunnerApplication.java
+++ tools/85b-swing-gui/src/main/java/gov/gsa/pivconformancegui/GuiRunnerApplication.java
@@ -87,6 +87,8 @@ public class GuiRunnerApplication {
 			}
 			apduAppender.rollover();
 		}
+		System.setProperty("sun.security.smartcardio.t0GetResponse", "false");
+		System.setProperty("sun.security.smartcardio.t1GetResponse", "false");
 		//Security.insertProviderAt(new jnasmartcardio.Smartcardio(), 1);
 
 		//Security.insertProviderAt(new de.intarsys.security.smartcard.smartcardio.SmartcardioProvider(), 1);
