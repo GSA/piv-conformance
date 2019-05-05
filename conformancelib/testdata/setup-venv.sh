9387098999757f756bfd0f881970d419aff21b68
Bob Fontana <bob.fontana@certipath.com>
2019-05-04 22:06:32 -0400

    Added data manipulation scripts

--- conformancelib/testdata/setup-venv.sh
+++ conformancelib/testdata/setup-venv.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/bash
 
-python36 -mvenv venv-xlrd
-source venv-xlrd/bin/activate
-pip install --upgrade pip
-pip install xlrd
-pip install xlwt
-pip install xlsxwriter
-source venv-xlrd/bin/activate
+UNAME=$(uname)
+CYG=$(expr $UNAME : CYG)
+
+cleanCRs() {
+	if [ $CYG -eq 3 ]; then
+		for F in $(find venv-xlrd -name '*.py'); do
+			sed -i.bak 's/\r$//' $F
+		done
+		sed -i.bak 's/\r$//' venv-xlrd/Scripts/activate
+	fi
+}
+
+python -mvenv venv-xlrd; cleanCRs
+source venv-xlrd/Scripts/activate
+pip install --upgrade pip; cleanCRs
+pip install xlrd; cleanCRs
+pip install xlwt; cleanCRS
+pip install xlsxwriter; cleanCRs
+source venv-xlrd/Scripts/activate
