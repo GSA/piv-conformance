6f10fbf6f364c682b6a3f66db1d3f0dc820a61ef
Manoj Srivastava <MKSCertiPath@users.noreply.github.com>
2019-05-09 23:50:08 -0400

    Updated unit tests to include all the split test atoms

--- conformancelib/src/main/java/gov/gsa/conformancelib/tests/SP800_73_4PrintedInfoTests.java
+++ conformancelib/src/main/java/gov/gsa/conformancelib/tests/SP800_73_4PrintedInfoTests.java
@@ -2,6 +2,7 @@ package gov.gsa.conformancelib.tests;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -37,7 +38,7 @@ public class SP800_73_4PrintedInfoTests {
 		assertTrue(bertlv.length <= 245, "Printed object length must be no larger than 245 bytes: got " + bertlv.length);
 	}
 
-	//Tags 0x01, 0x02, 0x05, 0x06 are present in that order
+	//Tags 0x01, 0x02, 0x05, 0x06 are present
 	@DisplayName("SP800-73-4.28 test")
     @ParameterizedTest(name = "{index} => oid = {0}")
     @MethodSource("sp800_73_4_PrintedInfoTestProvider")
@@ -60,15 +61,6 @@ public class SP800_73_4PrintedInfoTests {
 		assertTrue(tagList.contains(berPrintedInformationExpirationDateTag));
 		assertTrue(tagList.contains(berAgencyCardSerialTag));
 		assertTrue(tagList.contains(berIssuerIDTag));
-		
-		int orgIDTagIndex = tagList.indexOf(berNameTag);
-		
-		//Confirm tags 0x01, 0x02, 0x05, 0x06 are in right order
-		assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
-		assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
-		assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
-		assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
-		assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
     }
 	
 	//Tags 0x07 and 0x08 are optionally present in that order, following the tags from 73-4.28
@@ -129,7 +121,7 @@ public class SP800_73_4PrintedInfoTests {
 		}
 	}
 	
-	//Tag 0xFE is present and follows tags from 73-4.28, 73-4.29
+	//Tag 0xFE is present
 	@DisplayName("SP800-73-4.30 test")
 	@ParameterizedTest(name = "{index} => oid = {0}")
 	@MethodSource("sp800_73_4_PrintedInfoTestProvider")
@@ -139,93 +131,10 @@ public class SP800_73_4PrintedInfoTests {
 		
 		List<BerTag> tagList = ((PrintedInformation) o).getTagList();
 		
-		BerTag berNameTag = new BerTag(TagConstants.NAME_TAG);
-		BerTag berOrgAffiliationTag = new BerTag(TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG);
-		BerTag berOrgAffiliationL2Tag = new BerTag(TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG);
 		BerTag berECTag = new BerTag(TagConstants.ERROR_DETECTION_CODE_TAG);
-		
-		//Make sure Name tag is present
-		assertTrue(tagList.contains(berNameTag));
-		//Get index of the name tag
-		int orgIDTagIndex = tagList.indexOf(berNameTag);
-		
 		
 		//Make sure EDC tag is present
 		assertTrue(tagList.contains(berECTag));
-		
-		boolean optionalPresent = false;
-		
-		//Check the order to make sure EDC tag is last
-		if(tagList.contains(berOrgAffiliationTag)) {
-			
-			optionalPresent = true;
-			
-			//If organizational affiliation L2 tag is present check the order
-			if(tagList.contains(berOrgAffiliationL2Tag)) {
-				
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+7).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
-			
-			} else {
-			
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
-			}
-		}
-		
-		
-		//If organizational affiliation L2 tag is present check the order
-		if(tagList.contains(berOrgAffiliationL2Tag)) {
-			
-			optionalPresent = true;
-
-			//Different conditions if organizational affiliation is also present 
-			if(tagList.contains(berOrgAffiliationTag)) {
-				
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+7).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
-			
-			} else {
-				
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG));	
-				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));			
-			
-			}
-		}
-		
-		//If no optional tags are present check the order
-		if(optionalPresent == false) {
-			
-			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
-			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
-			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
-			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
-			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
-			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
-		}
-		
 	}
 	
 	//No tags other than (0x01, 0x02, 0x05, 0x06, 0x07, 0x08, 0xFE) are present
@@ -260,14 +169,52 @@ public class SP800_73_4PrintedInfoTests {
 	@MethodSource("sp800_73_4_PrintedInfoTestProvider")
 	@DisplayName("SP800-73-4.52 test")
 	void sp800_73_4_Test_52 (String oid, TestReporter reporter) {
-		
-		PIVDataObject o = AtomHelper.getDataObject(oid);
-		
-		// The first of up to 2 allowed assertions
-		assertTrue(o.decode(), "Couldn't decode " + oid);
-		
-		// TODO: Assert something meaningful here
-		assertTrue(o.getBytes().length >= 0, "Length is < 0");
+		try {
+			PIVDataObject o = AtomHelper.getDataObjectWithAuth(oid);
+			
+			//Get tag list
+			List<BerTag> tagList = ((PrintedInformation) o).getTagList();
+			
+			BerTag berNameTag = new BerTag(TagConstants.NAME_TAG);
+			BerTag berEmployeeAffiliationTag = new BerTag(TagConstants.EMPLOYEE_AFFILIATION_TAG);
+			BerTag berPrintedInformationExpirationDateTag = new BerTag(TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG);
+			BerTag berAgencyCardSerialTag = new BerTag(TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG);
+			BerTag berIssuerIDTag = new BerTag(TagConstants.ISSUER_IDENTIFICATION_TAG);
+			
+			//Confirm tags 0x01, 0x02, 0x05, 0x06 are present
+			if (tagList.contains(berNameTag) == false){
+				Exception e = new Exception("NAME_TAG is missing in tagList");
+				throw e;
+			}
+			if (tagList.contains(berEmployeeAffiliationTag) == false) {
+				Exception e = new Exception("EMPLOYEE_AFFILIATION_TAG is missing in tagList");
+				throw e;
+			}
+			if (tagList.contains(berPrintedInformationExpirationDateTag) == false) {
+				Exception e = new Exception("PRINTED_INFORMATION_EXPIRATION_DATE_TAG is missing in tagList");
+				throw e;
+			}
+			if (tagList.contains(berAgencyCardSerialTag) == false) {
+				Exception e = new Exception("AGENCY_CARD_SERIAL_NUMBER_TAG is missing in tagList");
+				throw e;
+			}
+			if (tagList.contains(berIssuerIDTag) == false) {
+				Exception e = new Exception("ISSUER_IDENTIFICATION_TAG is missing in tagList");
+				throw e;
+			}
+			
+			int orgIDTagIndex = tagList.indexOf(berNameTag);
+			
+			//Confirm tags 0x01, 0x02, 0x05, 0x06 are in right order
+			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
+			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
+			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
+			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
+			assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
+		}
+		catch (Exception e) {
+			fail(e);
+		}
 	}
 
 	// Tag 0xFE follows Tag 0x06, or optional Tags 0x07 or 0x08 (split from 73-4.30)
@@ -275,14 +222,107 @@ public class SP800_73_4PrintedInfoTests {
 	@MethodSource("sp800_73_4_PrintedInfoTestProvider")
 	@DisplayName("SP800-73-4.53 test")
 	void sp800_73_4_Test_53 (String oid, TestReporter reporter) {
-		
-		PIVDataObject o = AtomHelper.getDataObject(oid);
-		
-		// The first of up to 2 allowed assertions
-		assertTrue(o.decode(), "Couldn't decode " + oid);
-		
-		// TODO: Assert something meaningful here
-		assertTrue(o.getBytes().length >= 0, "Length is < 0");
+		try {
+			PIVDataObject o = AtomHelper.getDataObjectWithAuth(oid);
+			
+			List<BerTag> tagList = ((PrintedInformation) o).getTagList();
+			
+			BerTag berNameTag = new BerTag(TagConstants.NAME_TAG);
+			BerTag berOrgAffiliationTag = new BerTag(TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG);
+			BerTag berOrgAffiliationL2Tag = new BerTag(TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG);
+			BerTag berECTag = new BerTag(TagConstants.ERROR_DETECTION_CODE_TAG);
+			
+			//Make sure Name tag is present
+			if (tagList.contains(berNameTag) == false) {
+				Exception e = new Exception("NAME_TAG is missing");
+				throw e;
+			}
+			//Get index of the name tag
+			int orgIDTagIndex = tagList.indexOf(berNameTag);
+			
+			
+			//Make sure EDC tag is present
+			if (tagList.contains(berECTag)==false) {
+				Exception e = new Exception("ERROR_DETECTION_CODE_TAG is missing");
+				throw e;
+			}
+			
+			boolean optionalPresent = false;
+			
+			//Check the order to make sure EDC tag is last
+			if(tagList.contains(berOrgAffiliationTag)) {
+				
+				optionalPresent = true;
+				
+				//If organizational affiliation L2 tag is present check the order
+				if(tagList.contains(berOrgAffiliationL2Tag)) {
+					
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+7).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
+				
+				} else {
+				
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
+				}
+			}
+			
+			
+			//If organizational affiliation L2 tag is present check the order
+			if(tagList.contains(berOrgAffiliationL2Tag)) {
+				
+				optionalPresent = true;
+	
+				//Different conditions if organizational affiliation is also present 
+				if(tagList.contains(berOrgAffiliationTag)) {
+					
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L1_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+7).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
+				
+				} else {
+					
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ORGANIZATIONAL_AFFILIATION_L2_TAG));	
+					assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+6).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));			
+				
+				}
+			}
+			
+			//If no optional tags are present check the order
+			if(optionalPresent == false) {
+				
+				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex).bytes,TagConstants.NAME_TAG));
+				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+1).bytes,TagConstants.EMPLOYEE_AFFILIATION_TAG));
+				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+2).bytes,TagConstants.PRINTED_INFORMATION_EXPIRATION_DATE_TAG));
+				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+3).bytes,TagConstants.AGENCY_CARD_SERIAL_NUMBER_TAG));
+				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+4).bytes,TagConstants.ISSUER_IDENTIFICATION_TAG));
+				assertTrue(Arrays.equals(tagList.get(orgIDTagIndex+5).bytes,TagConstants.ERROR_DETECTION_CODE_TAG));
+			}
+		}
+		catch (Exception e) {
+			fail(e);
+		}
 	}
 	
 	private static Stream<Arguments> sp800_73_4_PrintedInfoTestProvider() {
