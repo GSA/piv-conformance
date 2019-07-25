89644dcd3044c2aaa4e017fdf2f6c5dd2d306b63
Bob Fontana <bob.fontana@certipath.com>
2019-07-25 13:18:25 -0400

    Rename class field

--- cardlib/src/main/java/gov/gsa/pivconformance/card/client/DataModelSingleton.java
+++ cardlib/src/main/java/gov/gsa/pivconformance/card/client/DataModelSingleton.java
@@ -5,38 +5,77 @@ package gov.gsa.pivconformance.card.client;
 
 import gov.gsa.pivconformance.tlv.*;
 
+/**
+ * This class is used indirectly by the test atoms.  It instantiates some
+ * known SP 800-73 rules. Initially being used to manage the container lengths
+ * in Tables 8-43.
+ * 
+ * @author Bob.Fontana
+ *
+ */
+
 public class DataModelSingleton {
 
-	TagBoundaryManager m_containerLengthRules;
+	TagBoundaryManager m_tagLengthRules;
 	
     private DataModelSingleton() {
     	reset();
     }
     
+    /*
+     * The INSTANCE
+     */
+    
     private static final DataModelSingleton INSTANCE = new DataModelSingleton();
+    
+    /**
+     * Public accessor of this so it can be instantiated by the framework
+     * 
+     * @return
+     */
     
     public static DataModelSingleton getInstance() {
         return INSTANCE;
     }
     
+    /**
+     * Reset
+     */
+    
     public void reset() {
-    	m_containerLengthRules = null;
-    	m_containerLengthRules = new TagBoundaryManager();
+    	m_tagLengthRules = null;
+    	m_tagLengthRules = new TagBoundaryManager();
     }
+    
+    /**
+     * Initialize
+     */
  
 	public void loadLengthRules() {
-		if (m_containerLengthRules == null)
-			m_containerLengthRules = new TagBoundaryManager();
+		if (m_tagLengthRules == null)
+			m_tagLengthRules = new TagBoundaryManager();
 	}
+	
+	/**
+	 * Day-to-day public accessor
+	 * 
+	 * @return the SP 800-73-* tag length rules
+	 */
 	
 	public TagBoundaryManager getLengthRules() {
-		if (m_containerLengthRules == null)
+		if (m_tagLengthRules == null)
 			loadLengthRules();
 		
-		return m_containerLengthRules;
+		return m_tagLengthRules;
 	}
 	
+	/**
+	 * Setter
+	 * 
+	 * @param clf
+	 */
+	
 	public void setLengthRules(TagBoundaryManager clf) {
-		m_containerLengthRules = clf;
+		m_tagLengthRules = clf;
 	}
 }
