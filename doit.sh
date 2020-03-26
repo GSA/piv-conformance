183b8a5d4c8983db9d24ab163b361d80f3c7a7ea
Bob Fontana <bfontana@bfontanagroup.com>
2020-03-25 19:11:18 -0600

    Added warning

--- doit.sh
+++ doit.sh
@@ -2,21 +2,21 @@
 . ./tools/85b-swing-gui/ensuredeps.sh
 
 pushd cardlib >/dev/null 2>&1
-	./gradlew clean
-	./gradlew eclipse
-	./gradlew installSource
-	./gradlew install
+	./gradlew --warning-mode=all clean
+	./gradlew --warning-mode=all eclipse
+	./gradlew --warning-mode=all installSource
+	./gradlew --warning-mode=all install
 popd >/dev/null 2>&1
 
 pushd conformancelib >/dev/null 2>&1
-	./gradlew clean
-	./gradlew eclipse
-	./gradlew shadowJar
+	./gradlew --warning-mode=all clean
+	./gradlew --warning-mode=all eclipse
+	./gradlew --warning-mode=all shadowJar
 popd >/dev/null 2>&1
 
 pushd tools/85b-swing-gui 2>&1
 	./ensuredeps.sh
-	./gradlew clean
-	./gradlew eclipse
-	./gradlew shadowJar
+	./gradlew --warning-mode=all clean
+	./gradlew --warning-mode=all eclipse
+	./gradlew --warning-mode=all shadowJar
 popd >/dev/null 2>&1
