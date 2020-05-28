c6033cf4b6231f5d8388de5520eece17e360e566
Bob Fontana <bfontana@bfontanagroup.com>
2020-05-28 07:27:29 -0600

    Added to index

---
+++ conformancelib/src/main/java/gov/gsa/conformancelib/pivconformancetools/PathValidator.java
@@ -0,0 +1,120 @@
+package gov.gsa.conformancelib.pivconformancetools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.SignatureException;
+import java.security.cert.CRL;
+import java.security.cert.CRLException;
+import java.security.cert.CertPath;
+import java.security.cert.CertPathBuilder;
+import java.security.cert.CertPathBuilderException;
+import java.security.cert.CertPathBuilderResult;
+import java.security.cert.CertStore;
+import java.security.cert.CertStoreParameters;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.CollectionCertStoreParameters;
+import java.security.cert.PKIXBuilderParameters;
+import java.security.cert.PKIXCertPathBuilderResult;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509CRL;
+import java.security.cert.X509CertSelector;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.bouncycastle.jcajce.provider.asymmetric.X509;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import gov.gsa.conformancelib.utilities.Utils;
+
+public class PathValidator {
+
+	private static PKIXCertPathBuilderResult buildCertPath(X509Certificate eeCert, String keystorePath, String keystorePass) 
+			throws KeyStoreException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, CertPathBuilderException {
+		PKIXCertPathBuilderResult result = null;
+		KeyStore keyStore = Utils.loadKeyStore(keystorePath, keystorePass);
+		X509Certificate commonPolicyCert = (X509Certificate) keyStore.getCertificate("federal common policy ca");
+		X509CertSelector target = new X509CertSelector();
+		target.setCertificate(eeCert);
+		PKIXBuilderParameters params = new PKIXBuilderParameters(keyStore, target);
+        // build the path
+        CertPathBuilder builder = null;
+        List<X509Certificate> list = new ArrayList<X509Certificate>();
+
+        try {
+	        list.add(commonPolicyCert);
+
+	        // Now, add intermediate certs to the list
+	        Set s = Utils.getIntermediateCerts(keyStore);
+	        for (X509Certificate c : Utils.getIntermediateCerts(keyStore)) {
+	        	list.add(c);
+	        }
+
+	        CollectionCertStoreParameters csParams = new CollectionCertStoreParameters(list);
+	        CertStore store = CertStore.getInstance("Collection", csParams, "BC");
+
+	        builder = CertPathBuilder.getInstance("PKIX", "BC");
+	        X509CertSelector endConstraints = new X509CertSelector();       
+	        endConstraints.setCertificate(eeCert);
+	        
+	        PKIXBuilderParameters buildParams = new PKIXBuilderParameters(Collections.singleton(new TrustAnchor(commonPolicyCert, null)), endConstraints);
+      
+	        buildParams.addCertStore(store);
+	        buildParams.setDate(new Date());
+        
+	        result = (PKIXCertPathBuilderResult) builder.build(buildParams);
+	        CertPath path = result.getCertPath();			
+
+	        Iterator<Certificate>  it = (Iterator<Certificate>) path.getCertificates().iterator();
+	        while (it.hasNext()) {
+	            System.out.println(((X509Certificate)it.next()).getSubjectX500Principal());
+	        }
+	        
+	        System.out.println(result.getTrustAnchor().getTrustedCert().getSubjectX500Principal());
+		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+			e.printStackTrace();
+		}
+ 
+		return result;
+	}
+
+	public static void main(String args[]) throws Exception {
+        // create certificates and CRLs
+		java.security.Security.addProvider(new BouncyCastleProvider());
+		String cwd = Utils.pathFixup(PathValidator.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+		System.out.println("Current directory is " + cwd + File.separator);
+		String keystorePath = args[0];
+		String keystorePass = args[1];
+		CertificateFactory eeCf = CertificateFactory.getInstance("X.509");
+		FileInputStream in1 = new FileInputStream(args[2]);
+		X509Certificate eeCert = (X509Certificate) eeCf.generateCertificate(in1);
+		in1.close();
+
+		PKIXCertPathBuilderResult cpbr = buildCertPath(eeCert, keystorePath, keystorePass);
+		if (cpbr != null) {
+			List<List<X509Certificate>> certPath = Utils.getCompleteCertChain(cpbr);
+			System.out.println(certPath.toString());
+		}		
+	}
+}
---
+++ conformancelib/src/main/java/gov/gsa/conformancelib/utilities/Utils.java
@@ -0,0 +1,525 @@
+package gov.gsa.conformancelib.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.PKIXCertPathBuilderResult;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509CRL;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.slf4j.LoggerFactory;
+
+public class Utils
+{
+	private static final org.slf4j.Logger s_logger = LoggerFactory.getLogger(Utils.class);;
+    private static final int VALIDITY_PERIOD = 7 * 24 * 60 * 60 * 1000; // one week
+    
+    /**
+     * Generate a sample V3 certificate to use as a CA root certificate
+     */
+    public static X509Certificate generateRootCert(String commonName, KeyPair pair)
+        throws Exception
+	{
+    	byte[] publicKey = pair.getPublic().getEncoded();
+    	SubjectPublicKeyInfo bcPublicKey = SubjectPublicKeyInfo.getInstance(publicKey);
+ 
+    	X509v3CertificateBuilder certGen = new X509v3CertificateBuilder(
+	        new X500Name(commonName),
+	        BigInteger.valueOf(1),
+	        new Date(System.currentTimeMillis()),
+	        (new Date(System.currentTimeMillis() + VALIDITY_PERIOD)),
+	        new X500Name(commonName),
+	        bcPublicKey
+    	);
+    	
+        certGen.addExtension(Extension.subjectKeyIdentifier, false, new JcaX509ExtensionUtils().createSubjectKeyIdentifier(pair.getPublic()));
+        certGen.addExtension(Extension.basicConstraints, true, new BasicConstraints(0));
+        certGen.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyAgreement | KeyUsage.keyEncipherment | KeyUsage.keyCertSign | KeyUsage.cRLSign));
+          
+    	X509CertificateHolder holder = certGen.build(new JcaContentSignerBuilder("SHA256withRSA").build(pair.getPrivate()));
+    	
+    	return new JcaX509CertificateConverter().setProvider( "BC" ).getCertificate(holder);
+	}
+    
+    /**
+     * Generate a sample V3 certificate to use as an intermediate CA certificate
+     */
+    public static X509Certificate generateIntermediateCert(String commonName, PublicKey intKey, PrivateKey caKey, X509Certificate caCert)
+        throws Exception
+    {
+    	SubjectPublicKeyInfo spki = SubjectPublicKeyInfo.getInstance(intKey.getEncoded());
+    	Principal issuerPrincipal = caCert.getSubjectX500Principal();
+    	X500Name issuerName = new X500Name(issuerPrincipal.getName()); 
+    	X509v3CertificateBuilder certGen = new X509v3CertificateBuilder(
+    		issuerName,
+	        BigInteger.valueOf(2),
+	        new Date(System.currentTimeMillis()),
+	        (new Date(System.currentTimeMillis() + VALIDITY_PERIOD)),
+	        new X500Name(commonName),
+	        spki
+    	);
+
+        certGen.addExtension(Extension.authorityKeyIdentifier, false, new JcaX509ExtensionUtils().createAuthorityKeyIdentifier(caCert));
+        certGen.addExtension(Extension.subjectKeyIdentifier, false, new JcaX509ExtensionUtils().createSubjectKeyIdentifier(intKey));
+        certGen.addExtension(Extension.basicConstraints, true, new BasicConstraints(0));
+        certGen.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyAgreement | KeyUsage.keyEncipherment | KeyUsage.keyCertSign | KeyUsage.cRLSign));
+    	
+    	X509CertificateHolder holder = certGen.build(new JcaContentSignerBuilder("SHA256withRSA").build(caKey));
+    	
+    	return new JcaX509CertificateConverter().setProvider( "BC" ).getCertificate(holder);
+    }
+    
+    /**
+     * Generate a sample V3 certificate to use as an end entity certificate
+     */
+    public static X509Certificate generateEndEntityCert(String commonName, PublicKey entityKey, PrivateKey caKey, X509Certificate caCert)
+	    throws Exception
+	{
+    	SubjectPublicKeyInfo bcPublicKey = SubjectPublicKeyInfo.getInstance(entityKey.getEncoded());
+    	Principal issuerPrincipal = caCert.getSubjectX500Principal();
+    	X500Name issuerName = new X500Name(issuerPrincipal.getName()); 
+    	X509v3CertificateBuilder certGen = new X509v3CertificateBuilder(
+    			issuerName, 
+    			BigInteger.valueOf(3),
+    			new Date(System.currentTimeMillis()),
+    			new Date(System.currentTimeMillis() + VALIDITY_PERIOD), 
+    			new X500Name(commonName),
+    			bcPublicKey);
+
+        certGen.addExtension(Extension.authorityKeyIdentifier, false, new JcaX509ExtensionUtils().createAuthorityKeyIdentifier(caCert));
+        certGen.addExtension(Extension.subjectKeyIdentifier, false, new JcaX509ExtensionUtils().createSubjectKeyIdentifier(entityKey));
+        certGen.addExtension(Extension.basicConstraints, true, new BasicConstraints(0));
+        certGen.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature));
+        
+    	X509CertificateHolder holder = certGen.build(new JcaContentSignerBuilder("SHA256withRSA").build(caKey));
+    	
+    	return new JcaX509CertificateConverter().setProvider( "BC" ).getCertificate(holder);
+	}
+    
+    /**
+     * Create a random 2048-bit RSA key pair
+     */
+    public static KeyPair generateRSAKeyPair() throws Exception
+	{
+        KeyPairGenerator  kpGen = KeyPairGenerator.getInstance("RSA", "BC");
+        kpGen.initialize(2048, new SecureRandom());
+        return kpGen.generateKeyPair();
+	}
+    
+    private static class FixedRand extends SecureRandom  {
+
+		private static final long serialVersionUID = 1L;
+		MessageDigest sha;
+        byte[] state;
+        
+        FixedRand()
+        {
+            try
+            {
+                this.sha = MessageDigest.getInstance("SHA-256");
+                this.state = sha.digest();
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+                throw new RuntimeException("can't find SHA-256!");
+            }
+        }
+	
+	    public void nextBytes(
+	       byte[] bytes)
+	    {
+	        int	off = 0;
+	        
+	        sha.update(state);
+	        
+	        while (off < bytes.length)
+	        {	            
+	            state = sha.digest();
+	            
+	            if (bytes.length - off > state.length)
+	            {
+	                System.arraycopy(state, 0, bytes, off, state.length);
+	            }
+	            else
+	            {
+	                System.arraycopy(state, 0, bytes, off, bytes.length - off);
+	            }
+	            
+	            off += state.length;
+	            
+	            sha.update(state);
+	        }
+	    }
+    }
+    
+    /**
+     * Returns a list of certificates in each chain identified in a PKIXCertPathBuilderResult object
+     * @param pkixCertPath the results of the path builder build method
+     * @return a list of certificates
+     * @throws CertificateException
+     */
+	public static List<List<X509Certificate>> getCompleteCertChain(PKIXCertPathBuilderResult pkixCertPath)
+            throws CertificateException {
+        List<List<X509Certificate>> certificateList = new ArrayList<>();
+
+        List<X509Certificate> certificatePath = new ArrayList<>();
+        X509Certificate rootCaCert = pkixCertPath.getTrustAnchor().getTrustedCert();
+		Collection<X509Certificate> collection =  (Collection<X509Certificate>) pkixCertPath.getCertPath().getCertificates();
+        certificatePath.addAll(collection);
+        certificatePath.add(rootCaCert);
+
+        int certificatesSize = certificatePath.size();
+        if (1 == certificatesSize) {
+            throw new CertificateException("Certificate Path insufficient size: must be at least 2");
+        }
+        for (int i = 0; i < certificatesSize; i++) {
+            if (certificatesSize - 1 == i) {  //We've reached the root CA
+                break;
+            } else {
+                List<X509Certificate> certTuple = new ArrayList<>(2);
+                certTuple.add((X509Certificate)certificatePath.get(i));
+                certTuple.add((X509Certificate)certificatePath.get(i+1));
+                certificateList.add(certTuple);
+            }
+        }
+        return certificateList;
+    } 
+
+	/**
+	 * Extracts a set of TrustAnchor objects from a KeyStore
+	 * @name getTrustAnchors
+	 * @c
+	 */
+	
+	/**
+	 * Gets a set of self-signed from a given keyStore
+	 * @param keyStore
+	 * @return Set of TrustAnchor
+	 */
+	public static HashSet<TrustAnchor> getTrustAnchors(KeyStore keyStore) {
+		
+		HashSet<TrustAnchor> result = new HashSet<TrustAnchor>();
+
+		Enumeration<String> aliases;
+		try {
+			aliases = keyStore.aliases();
+			while (aliases.hasMoreElements()) {
+				String alias = aliases.nextElement();
+				X509Certificate cert = (X509Certificate) keyStore.getCertificate(alias);
+	            PublicKey key = cert.getPublicKey();
+	            try {
+	            	cert.verify(key);
+	            	X509Certificate taCert = (X509Certificate) keyStore.getCertificate(alias);
+	            	TrustAnchor ta = new TrustAnchor(taCert, null);
+	            	result.add(ta);
+	            } catch (InvalidKeyException | SignatureException e) {
+	            	s_logger.error("Exception: " + e.getMessage());
+	            }
+			}
+		} catch (Exception e1) {
+        	s_logger.error("Exception: " + e1.getMessage());
+		}
+		return result;
+	}
+	
+	/**
+	 * Extract all non- self-signed certificates from the given KeyStore
+	 * 
+	 * @param KeyStore object containing the certificates
+	 * @return Set of X509Certificate
+	 */	
+    public static HashSet<X509Certificate> getIntermediateCerts(KeyStore keyStore) {
+		HashSet<X509Certificate> result = new HashSet<X509Certificate>();
+		Enumeration<String> aliases = null;
+		try {
+			aliases = keyStore.aliases();
+			while (aliases.hasMoreElements()) {
+				String alias = aliases.nextElement();
+				X509Certificate cert = (X509Certificate) keyStore.getCertificate(alias);
+	            PublicKey key = cert.getPublicKey();
+	            try {
+	            	cert.verify(key);
+	            } catch (InvalidKeyException | SignatureException e) {
+	            	result.add((X509Certificate) keyStore.getCertificate(alias));
+	            }
+			}	
+		} catch (Exception e) {
+        	s_logger.error("Exception: " + e.getMessage());
+		}
+		return result;
+	}
+    
+	/**
+	 * Load an X509CRL from the named file
+	 * 
+	 * @param path
+	 * @return the X509CRL
+	 */
+	private static X509CRL loadCRLFromFile(String path) {
+		X509CRL result = null;
+		FileInputStream in;
+		try {
+			in = new FileInputStream(path);
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			result = (X509CRL) cf.generateCRL(in);
+			in.close();
+		} catch (Exception e) {
+        	s_logger.error("Exception: " + e.getMessage());
+		}
+
+		return result;
+	}
+	
+	/**
+	 * Load an X509 certificate from the named file
+	 * 
+	 * @param path
+	 * @return the X509Certificate
+	 */
+	
+	public static X509Certificate loadCertFromFile(String path) {
+		X509Certificate result = null;
+		FileInputStream in;
+		try {
+			in = new FileInputStream(path);
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			result = (X509Certificate) cf.generateCertificate(in);
+			in.close();
+		} catch (CertificateException | IOException e) {
+        	s_logger.error("Exception: " + e.getMessage());
+		}
+
+		return result;
+	}
+	   
+	/**
+	 * Load a TrustAnchor object from a given keystore and password
+	 * 
+	 * @param keystorePath the location of the keystore 
+	 * @param keystorePass the password to the keystore
+	 * @return a TrustAnchor object
+	 * @throws KeyStoreException
+	 */
+	public static KeyStore loadKeyStore(String keystorePath, String keystorePass) throws KeyStoreException {
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		try (InputStream in = new FileInputStream(keystorePath)) {
+			keyStore.load(in, keystorePass.toCharArray());
+		} catch (CertificateException | NoSuchAlgorithmException | IOException e) {
+        	s_logger.error("Exception: " + e.getMessage());
+		}
+
+		return keyStore;
+	}
+	
+	/**
+	 * Corrects the path separators for a path.
+	 * 
+	 * @param inPath the path as read from a configuration file, etc.
+	 * @return a path with the correct path separators for the local OS
+	 */
+ 
+	public static String pathFixup(String inPath) {
+		boolean windowsOs = false;
+		String osName = System.getProperty("os.name");
+
+		if (osName.toLowerCase().contains("windows")) {
+			windowsOs = true;
+		}
+
+		String outPath = inPath;
+		if (windowsOs == true) {
+			if (inPath.contains("/")) {
+				outPath = inPath.replace("/", "\\");
+			}
+		} else if (inPath.contains("\\")) {
+			outPath = inPath.replace("\\", "/");
+		}
+
+		return outPath;
+	}	
+	
+	/**
+     * Return a SecureRandom which produces the same value.
+     * 
+     * <b>This is for testing only!</b>
+     * @return a fixed random
+     */
+    public static SecureRandom createFixedRandom()
+    {
+        return new FixedRand();
+    }
+    
+    private static String	digits = "0123456789abcdef";
+    
+    /**
+     * Return length many bytes of the passed in byte array as a hex string.
+     * 
+     * @param data the bytes to be converted.
+     * @param length the number of bytes in the data block to be converted.
+     * @return a hex representation of length bytes of data.
+     */
+    public static String toHex(byte[] data, int length)
+    {
+        StringBuffer	buf = new StringBuffer();
+        
+        for (int i = 0; i != length; i++)
+        {
+            int	v = data[i] & 0xff;
+            
+            buf.append(digits.charAt(v >> 4));
+            buf.append(digits.charAt(v & 0xf));
+        }
+        
+        return buf.toString();
+    }
+    
+    /**
+     * Return the passed in byte array as a hex string.
+     * 
+     * @param data the bytes to be converted.
+     * @return a hex representation of data.
+     */
+    public static String toHex(byte[] data)
+    {
+        return toHex(data, data.length);
+    }
+    
+    /**
+     * Create a key for use with AES.
+     * 
+     * @param bitLength
+     * @param random
+     * @return an AES key.
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchProviderException
+     */
+    public static SecretKey createKeyForAES(int bitLength, SecureRandom random)
+        throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        KeyGenerator generator = KeyGenerator.getInstance("AES", "BC");
+        
+        generator.init(256, random);
+        
+        return generator.generateKey();
+    }
+    
+    /**
+     * Create an IV suitable for using with AES in CTR mode.
+     * <p>
+     * The IV will be composed of 4 bytes of message number,
+     * 4 bytes of random data, and a counter of 8 bytes.
+     * 
+     * @param messageNumber the number of the message.
+     * @param random a source of randomness
+     * @return an initialised IvParameterSpec
+     */
+    public static IvParameterSpec createCtrIvForAES(int messageNumber, SecureRandom random) {
+        byte[] ivBytes = new byte[16];
+        
+        // initially randomize
+        
+        random.nextBytes(ivBytes);
+        
+        // set the message number bytes
+        
+        ivBytes[0] = (byte)(messageNumber >> 24);
+        ivBytes[1] = (byte)(messageNumber >> 16);
+        ivBytes[2] = (byte)(messageNumber >> 8);
+        ivBytes[3] = (byte)(messageNumber >> 0);
+        
+        // set the counter bytes to 1
+        
+        for (int i = 0; i != 7; i++)
+        {
+            ivBytes[8 + i] = 0;
+        }
+        
+        ivBytes[15] = 1;
+        
+        return new IvParameterSpec(ivBytes);
+    }
+    
+    /**
+     * Convert a byte array of 8 bit characters into a String.
+     * 
+     * @param bytes the array containing the characters
+     * @param length the number of bytes to process
+     * @return a String representation of bytes
+     */
+    public static String toString( byte[] bytes, int length) {
+        char[]	chars = new char[length];
+        
+        for (int i = 0; i != chars.length; i++)
+        {
+            chars[i] = (char)(bytes[i] & 0xff);
+        }
+        
+        return new String(chars);
+    }
+    
+    /**
+     * Convert a byte array of 8 bit characters into a String.
+     * 
+     * @param bytes the array containing the characters
+     * @return a String representation of bytes
+     */
+    public static String toString(byte[] bytes) {
+        return toString(bytes, bytes.length);
+    }
+    
+    /**
+     * Convert the passed in String to a byte array by
+     * taking the bottom 8 bits of each character it contains.
+     * 
+     * @param string the string to be converted
+     * @return a byte array representation
+     */
+    public static byte[] toByteArray(String string) {
+        byte[]	bytes = new byte[string.length()];
+        char[]  chars = string.toCharArray();
+        
+        for (int i = 0; i != chars.length; i++)
+        {
+            bytes[i] = (byte)chars[i];
+        }
+        
+        return bytes;
+    }
+}
