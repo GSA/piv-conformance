f0968bfe5475767bdf22bb0a42bc036792cba31c
bob-fontana <bfontana@bfontanagroup.com>
2020-09-15 22:26:02 -0600

    Refactored and renamed

--- conformancelib/src/main/java/gov.gsa.pivconformance/conformancelib/junitoptions/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Configuration options for JUnit's console launcher.
- */
-
-package gov.gsa.pivconformance.conformancelib.junitoptions;
--- conformancelib/src/main/java/gov.gsa.pivconformance/conformancelib/tools/junitconsole/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Internal execution tasks for JUnit's console launcher.
- */
-
-package gov.gsa.pivconformance.conformancelib.tools.junitconsole;
