00022108a5d95532a456904fe54f5ea2bf302124
bob-fontana <bfontana@bfontanagroup.com>
2020-09-20 10:34:28 -0600

    Organized Apache CLI dependency

--- cardlib/src/main/java/module-info.java
+++ cardlib/src/main/java/module-info.java
@@ -1,13 +1,14 @@
-open module gov.gsa.pivconformance.cardlib {
-    requires java.smartcardio;
-    requires java.sql;
-    requires org.slf4j;
-    requires org.apache.commons.codec;
-    requires org.apache.commons.cli;
-    requires org.bouncycastle.pkix;
-    requires org.bouncycastle.provider;
-    exports gov.gsa.pivconformance.cardlib.tlv;
-    exports gov.gsa.pivconformance.cardlib.card.client;
-    exports gov.gsa.pivconformance.cardlib.tools;
-    exports gov.gsa.pivconformance.cardlib.utils;
+module gov.gsa.pivconformance.cardlib {
+	exports gov.gsa.pivconformance.cardlib.card.client;
+	exports gov.gsa.pivconformance.cardlib.tools;
+	exports gov.gsa.pivconformance.cardlib.utils;
+	exports gov.gsa.pivconformance.cardlib.tlv;
+
+	requires org.apache.commons.cli;
+	requires java.smartcardio;
+	requires java.sql;
+	requires org.apache.commons.codec;
+	requires org.bouncycastle.pkix;
+	requires org.bouncycastle.provider;
+	requires org.slf4j;
 }
