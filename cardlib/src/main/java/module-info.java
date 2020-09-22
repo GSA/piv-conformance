f61bef8df3bd7f8bf5e936f12840b077509b3250
bob-fontana <bfontana@bfontanagroup.com>
2020-09-21 23:47:57 -0600

    Updated dependencies

--- cardlib/src/main/java/module-info.java
+++ cardlib/src/main/java/module-info.java
@@ -4,9 +4,9 @@ module gov.gsa.pivconformance.cardlib {
 	exports gov.gsa.pivconformance.cardlib.utils;
 	exports gov.gsa.pivconformance.cardlib.tlv;
 
-	requires org.apache.commons.cli;
 	requires java.smartcardio;
 	requires java.sql;
+	requires org.apache.commons.cli;
 	requires org.apache.commons.codec;
 	requires org.bouncycastle.pkix;
 	requires org.bouncycastle.provider;
