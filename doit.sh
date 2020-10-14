652c25ab950e3a9d1d2dcf903b3659bfd89a8ac5
Bob Fontana <bfontana@bfontanagroup.com>
2020-10-14 17:08:51 +0100

    Updated

--- doit.sh
+++ doit.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-. ./tools/85b-swing-gui/ensuredeps.sh
 
 pushd cardlib >/dev/null 2>&1
-	./gradlew --warning-mode=all clean
-	./gradlew --warning-mode=all eclipse
-	./gradlew --warning-mode=all installSource
-	./gradlew --warning-mode=all install
+	./gradlew -x junitPlatformTest -x generateHtmlTestReports clean build install
 popd >/dev/null 2>&1
 
 pushd conformancelib >/dev/null 2>&1
-	./gradlew --warning-mode=all clean
-	./gradlew --warning-mode=all eclipse
-	./gradlew --warning-mode=all shadowJar
+	./gradlew -x test clean build install
 popd >/dev/null 2>&1
 
 pushd tools/85b-swing-gui 2>&1
-	./ensuredeps.sh
-	./gradlew --warning-mode=all clean
-	./gradlew --warning-mode=all eclipse
-	./gradlew --warning-mode=all shadowJar
+	./gradlew -x test clean build
+	cp build/libs/*shadow* ../../libs
 popd >/dev/null 2>&1
+
