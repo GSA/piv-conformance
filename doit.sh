518d740a58a94fa688def124feb2e591b4309e04
bob-fontana <bfontana@bfontanagroup.com>
2020-10-15 20:47:21 -0600

    Target directory setup

--- doit.sh
+++ doit.sh
@@ -18,9 +18,8 @@ rm -rf fips201-card-conformance-tool-$VERSION
 mkdir -p fips201-card-conformance-tool-$VERSION
 pushd fips201-card-conformance-tool-$VERSION >/dev/null 2>&1
 	cp -p ../conformancelib/testdata/*.db .
-	cp -p ../tools/85b-swing-gui/user_log_config.xml .
+	cp -p ../tools/85b-swing-gui/build/resources/main/user_log_config.xml .
+	cp -p ../tools/85b-swing-gui/build/resources/main/build.version .
 	tar xvf ../tools/85b-swing-gui/build/distributions/gov.gsa.pivconformance.gui-shadow-$VERSION.tar
 	mv gov.gsa.pivconformance.gui-shadow-$VERSION/lib/gov.gsa.pivconformance.gui-$VERSION-shadow.jar .
-	mkdir -p resources build/resources 
-	cp -pr ../tools/85b-swing-gui/build/resources .
 popd
