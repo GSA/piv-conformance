742d77c27c63fd77fc2dec06c8c08a5e549c9f3a
bob-fontana <bfontana@bfontanagroup.com>
2020-10-23 13:28:26 -0600

    Added test resources and .gitignore

--- conformancelib/src/test/java/gov/gsa/pivconformance/conformancelib/test/ValidatorTest.java
+++ conformancelib/src/test/java/gov/gsa/pivconformance/conformancelib/test/ValidatorTest.java
@@ -48,43 +48,18 @@ class ValidatorTest {
             e.printStackTrace();
         }
         return ks;
-        /*
-        KeyStore ks = null;
-        System.out.println("Looking for keystore file " + keyStoreFile.getAbsolutePath());
-        try {
-            if (!keyStoreFile.exists()) {
-                URL url = ValidatorTest.class.getClassLoader().getResource("/cacerts.keystore");
-                System.out.println("URL.getFile() " + url.getFile());
-                System.out.println("URL.getPath()" + url.getPath());
-                String currentDir = System.getProperty("user.dir");
-                System.out.println("currentDir: " + currentDir);
-                keyStoreFile = new File(url.getPath());
-                if (!keyStoreFile.exists()) {
-                    System.out.println("Unable to locate " + keyStoreFile.getAbsolutePath());
-                    return ks;
-                }
-            }
-            FileInputStream fis;
-
-            ks = KeyStore.getInstance("JKS");
-            fis = new FileInputStream(keyStoreFile.getAbsolutePath());
-            ks.load(fis, "changeit".toCharArray());    
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return ks;
-         */
     }
 
     @DisplayName("Certificate Path Validation")
     @ParameterizedTest(name = "{index} => oid = {0}, file = {1}")
     @MethodSource("positiveCaseCertProvider")
     public void testIsValid(String oid, String endEntityCertFile, TestReporter reporter) {
-        final Path certsDir = Path.of("", "x509-certs");
+        final Path certsDir = Path.of("", "x509-certs/valid");
         try {
             System.out.println("cacerts.keystore contains alias icam test card root ca: " + m_keystore.containsAlias("icam test card root ca"));
             CertificateFactory fac = CertificateFactory.getInstance("X509");
-            X509Certificate eeCert = (X509Certificate) fac.generateCertificate(new FileInputStream(endEntityCertFile));
+            FileInputStream fis = (FileInputStream) Validator.getFileFromResourceAsStream(ValidatorTest.class, "x509-certs/valid" + File.separator + endEntityCertFile);
+            X509Certificate eeCert = (X509Certificate) fac.generateCertificate(new FileInputStream("x509-certs/valid" + File.separator +endEntityCertFile));
             X509Certificate trustAnchorCert = getTrustAnchorForGivenCertificate(certsDir, eeCert);
                    
             System.out.print("Validating " + eeCert.getSubjectDN().getName());
@@ -98,25 +73,7 @@ class ValidatorTest {
             e.printStackTrace();
         }
     }
-/*
-    // Credit: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-    // get a file from the resources folder
-    // works everywhere, IDEA, unit test and JAR file.
-    private InputStream getFileFromResourceAsStream(String fileName) {
 
-        // The class loader that loaded the class
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(fileName);
-
-        // the stream holding the file content
-        if (inputStream == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return inputStream;
-        }
-
-    }
- */
     private static Stream<Arguments> positiveCaseCertProvider() {
         final String policyFileName = "x509-certs/valid/policy.xml";
         try {
