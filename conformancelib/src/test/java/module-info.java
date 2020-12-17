c26892aab31aba4a23c1e5098a08968134f5dbe2
Bob Fontana <bfontana@bfontanagroup.com>
2020-12-17 09:57:05 -0700

    Removed exports, add requires

--- conformancelib/src/test/java/module-info.java
+++ conformancelib/src/test/java/module-info.java
@@ -1,6 +1,7 @@
 open module gov.gsa.pivconformance.conformancelib.test {
     exports gov.gsa.pivconformance.conformancelib.test;
     requires gov.gsa.pivconformance.cardlib;
+    requires gov.gsa.pivconformance.conformancelib;
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.params;
 }
