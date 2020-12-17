644d99d9227d6c17c1b30f26ec9cc594ddf7db43
Bob Fontana <bfontana@bfontanagroup.com>
2020-12-17 08:15:53 -0700

    Cleanup

--- conformancelib/src/test/java/module-info.java
+++ conformancelib/src/test/java/module-info.java
@@ -1,6 +1,6 @@
 open module gov.gsa.pivconformance.conformancelib.test {
     exports gov.gsa.pivconformance.conformancelib.test;
-    requires gov.gsa.pivconformance.conformancelib;
+    requires gov.gsa.pivconformance.cardlib;
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.params;
 }
