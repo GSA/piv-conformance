97161e259e4071c83d0b4bd9fcd60f58267da082
Bob Fontana <bfontana@bfontanagroup.com>
2021-02-04 14:02:48 -0700

    Removed unused enum

--- conformancelib/src/main/java/gov/gsa/pivconformance/conformancelib/utilities/ValidatorHelper.java
+++ conformancelib/src/main/java/gov/gsa/pivconformance/conformancelib/utilities/ValidatorHelper.java
@@ -23,27 +23,8 @@ import static gov.gsa.pivconformance.conformancelib.utilities.TestRunLogControll
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ValidatorHelper {
+
     private static final Logger s_logger = LoggerFactory.getLogger(ValidatorHelper.class);
-    public enum PolicyOID {
-        ID_FPKI_CERTPCY_PIVI_HARDWARE("2.16.840.1.101.3.2.1.3.18"),
-        ID_FPKI_COMMON_HARDWARE("2.16.840.1.101.3.2.1.3.7"),
-        ID_FPKI_COMMON_POLICY("2.16.840.1.101.3.2.1.3.6"),
-        ID_FPKI_CERTPCY_BASICASSURANCE("2.16.840.1.101.3.2.1.3.2"),
-        TEST_ID_FPKI_COMMON_CARDAUTH("2.16.840.1.101.3.2.1.48.13"),
-        TEST_ID_FPKI_COMMON_AUTHENTICATION("2.16.840.1.101.3.2.1.48.11"),
-        TEST_ID_FPKI_COMMON_HARDWARE("2.16.840.1.101.3.2.1.48.9"),
-        TEST_ID_FPKI_CERTPCY_MEDIUMHARDWARE("2.16.840.1.101.3.2.1.48.4");
-
-        private final String value;
-
-        PolicyOID(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
-    }
 
     public static X509Certificate getX509CertificateFromPath(String fullPathName) throws ConformanceTestException {
         String v_fullPathName = TestRunLogController.pathFixup(fullPathName);
