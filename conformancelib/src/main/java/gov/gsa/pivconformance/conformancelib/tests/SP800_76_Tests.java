dba90cae59ed876f0fe3e64edb669599b0a3c874
Clayton J Barnette <110616975+claytonjbarnette@users.noreply.github.com>
2023-11-16 13:58:38 -0500

    Update SP800_76_Tests.java
    Issue #315 - Added submitted fix as is to file.

--- conformancelib/src/main/java/gov/gsa/pivconformance/conformancelib/tests/SP800_76_Tests.java
+++ conformancelib/src/main/java/gov/gsa/pivconformance/conformancelib/tests/SP800_76_Tests.java
@@ -1171,7 +1171,10 @@ public class SP800_76_Tests {
 	        
 			byte [] zeroBlock = { 0x00, 0x00 };
 			
-			byte [] extendedDataBlockLength  = Arrays.copyOfRange(biometricDataBlock, offset+numberOfMinutiae*6+3, offset+numberOfMinutiae*6+3+2);
+			// byte [] extendedDataBlockLength  = Arrays.copyOfRange(biometricDataBlock, offset+numberOfMinutiae*6+3, offset+numberOfMinutiae*6+3+2);
+			// The line below was added from Issue 314 submitted by baud001, ASR(as submitted or requested). 
+			// Testing needs to be done to see if this resolves the issue. 
+			Arrays.copyOfRange(biometricDataBlock, offset+numberOfMinutiae*6+4, offset+numberOfMinutiae*6+4+2);
 			
 			assertTrue(Arrays.equals(extendedDataBlockLength, zeroBlock), "Extended data block length is not 0");
 			
